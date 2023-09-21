583af66bba54f11cfbece5a8d74c224c9fd95046
Yavuz Yalçınkaya <recepyavuzyalcinkaya@gmail.com>
2023-09-21 17:44:18 +0300

    update searchbar

--- src/components/header/Searchbar.vue
+++ src/components/header/Searchbar.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex mt-5 relative">
     <input
+      ref="searchBox"
       type="text"
       class="rounded-full bg-gray-700 px-8 py-2 w-48 mr-3 focus:outline-none text-white"
       placeholder="Film Ara..."
       @input="debounceSearch"
       v-model="searchTerm"
+      @focus="handleFocus"
     />
     <div class="absolute top-2 left-2 text-gray-300">
       <i class="fas fa-search fill-current w-4"></i>
@@ -14,13 +16,14 @@
     <div
       class="absolute bg-gray-800 w-60 rounded shadow-md z-10 overflow-y-auto mt-12"
     >
-      <ul v-if="searchResult.length !== 0" class="mt-2">
+      <ul v-if="showSearchResult" class="mt-2 max-h-[400px]">
         <router-link
           :to="'/movie/' + movie.id"
           tag="li"
           :key="movie.id"
           v-for="movie in searchResult"
-          class="flex items-center p-2 hover:bg-gray-700 cursor-pointer"
+          @click.native="showSearchResult = false"
+          class="flex items-center p-2 hover:bg-gray-700 cursor-pointer border-b border-white"
         >
           <img
             :src="posterPath(movie.poster_path)"
@@ -30,7 +33,7 @@
           <span class="ml-2 text-white">{{ movie.title }}</span>
         </router-link>
       </ul>
-      <ul class="px-3">
+      <ul class="px-3 z-10" v-else>
         <li v-if="noResultFound">No Result Found for "{{ searchTerm }}"</li>
       </ul>
     </div>
@@ -47,10 +50,14 @@ export default {
   data() {
     return {
       searchResult: [],
-      noResultFound: false,
       searchTerm: "",
+      showSearchResult: false,
+      noResultFound: false,
       debounce: null, // debounce işlemi için timer değişkeni
     };
+  },
+  mounted() {
+    this.keyboardEvents();
   },
   methods: {
     debounceSearch(event) {
@@ -61,6 +68,7 @@ export default {
         } else {
           this.searchResult = [];
           this.noResultFound = false;
+          this.showSearchResult = false; // Boş metinle arama yapıldığında sonuçları gizle
         }
       }, 600);
     },
@@ -68,10 +76,38 @@ export default {
       try {
         const response = await this.$http.get("/search/movie?query=" + term);
         this.searchResult = response?.data?.results || [];
+        this.showSearchResult = this.searchResult.length > 0; // Sonuçlar geldiyse sonuçları göster
         this.noResultFound = this.searchResult.length === 0;
       } catch (error) {
         console.log(error);
       }
+    },
+
+    handleFocus() {
+      if (this.searchTerm != "") {
+        this.showSearchResult = true;
+      }
+    },
+    keyboardEvents() {
+      let windowObj = this;
+
+      window.addEventListener("click", (e) => {
+        if (!this.$el.contains(e.target)) {
+          this.showSearchResult = false;
+        }
+      });
+
+      window.addEventListener("keypress", (e) => {
+        if (e.key === "/") {
+          e.preventDefault();
+          windowObj.$refs.searchBox.focus();
+        }
+      });
+      window.addEventListener("keydown", (e) => {
+        if (e.key == "Escape") {
+          this.showSearchResult = false;
+        }
+      });
     },
     posterPath(poster_path) {
       if (poster_path) {
